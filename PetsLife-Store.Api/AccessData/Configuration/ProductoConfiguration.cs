28b0be27f5c23e1aefeefd66da96efc078dc8690
dani <daniel.g7895@gmail.com>
2021-06-02 17:42:52 -0300

    update cors and added better products examples

--- PetsLife-Store.Api/AccessData/Configuration/ProductoConfiguration.cs
+++ PetsLife-Store.Api/AccessData/Configuration/ProductoConfiguration.cs
@@ -18,11 +18,13 @@ namespace AccessData.Configuration
 
             builder.HasData(new Producto[]
             {
-                new() {Nombre = "Pipeta", CantidadStock = 4, Categoria = "Farmacia", Imagen = "imagen.png", Precio = 2549, ProductoId = 1, TiendaId = 1},
-                new() {Nombre = "Dow Chow 20 kg", CantidadStock = 1, Categoria = "Alimento", Imagen = "dogchow.png", Precio = 6931, ProductoId = 2, TiendaId = 1},
-                new() {Nombre = "Cat Chow 15 kg", CantidadStock = 77, Categoria = "Alimento", Imagen = "catchow.png", Precio = 2350, ProductoId = 3, TiendaId = 1},
-                new() {Nombre = "Collar anti pulgas", CantidadStock = 12, Categoria = "Accesorios", Imagen = "antipulgas.png", Precio = 350, ProductoId = 4, TiendaId = 1},
-                new() {Nombre = "Torre gatos 2 metros", CantidadStock = 32, Categoria = "Accesorios", Imagen = "torregatos.png", Precio = 23500, ProductoId = 5, TiendaId = 1}
+                new() {Nombre = "Pipeta gatos", CantidadStock = 4, Categoria = "Farmacia", Imagen = "https://cdn.discordapp.com/attachments/790717330447138847/849714591042109510/unknown.png", Precio = 2549, ProductoId = 1, TiendaId = 1},
+                new() {Nombre = "Dow Chow 20 kg - Carne", CantidadStock = 1, Categoria = "Alimento", Imagen = "https://cdn.discordapp.com/attachments/790717330447138847/849734389634039848/unknown.png", Precio = 6931, ProductoId = 2, TiendaId = 1},
+                new() {Nombre = "Cat Chow 20 kg - Pescado", CantidadStock = 77, Categoria = "Alimento", Imagen = "https://cdn.discordapp.com/attachments/790717330447138847/849734518260105246/unknown.png", Precio = 2350, ProductoId = 3, TiendaId = 1},
+                new() {Nombre = "Collar Anti Pulgas Y Garrapatas Ultrasónico Perros Y Gatos", CantidadStock = 12, Categoria = "Accesorios", Imagen = "https://cdn.discordapp.com/attachments/790717330447138847/849735109976522809/unknown.png", Precio = 350, ProductoId = 4, TiendaId = 1},
+                new() {Nombre = "Rascador Para Gato Torre Felpa + Sisal", CantidadStock = 32, Categoria = "Accesorios", Imagen = "https://cdn.discordapp.com/attachments/790717330447138847/849735366385598474/unknown.png", Precio = 23500, ProductoId = 5, TiendaId = 1},
+                new() {Nombre = "Cama circular para Gato - Marron oscuro", CantidadStock = 12, Categoria = "Accesorios", Imagen = "https://cdn.discordapp.com/attachments/790717330447138847/849735627150589952/unknown.png", Precio = 23500, ProductoId = 6, TiendaId = 1},
+                new() {Nombre = "Juguete Gato Cañita Varita Cascabel Raton Plumas Extensible", CantidadStock = 12, Categoria = "Accesorios", Imagen = "https://cdn.discordapp.com/attachments/790717330447138847/849735929639206912/unknown.png", Precio = 197, ProductoId = 7, TiendaId = 1}
             });
 
         }
